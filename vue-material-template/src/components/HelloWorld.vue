04d1a1a894bc5a34e7b80f473c73fb89d1fa8585
Miguel Caceres <mcaceres@uni-bremen.de>
2020-06-23 22:37:02 +0200

    Add Vue material UI

---
+++ vue-material-template/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <md-field>
+      <label>Enter your text</label>
+      <md-input type="text"></md-input>
+    </md-field>
+    <md-button class="md-primary md-raised">Primary</md-button>
+    <md-card md-with-hover>
+      <md-ripple>
+        <md-card-header>
+          <div class="md-title">Card with hover effect</div>
+          <div class="md-subhead">It also have a ripple</div>
+        </md-card-header>
+
+        <md-card-content>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.</md-card-content>
+
+        <md-card-actions>
+          <md-button>Action</md-button>
+          <md-button>Action</md-button>
+        </md-card-actions>
+      </md-ripple>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
